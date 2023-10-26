4bb3981f91529ceb72bf6411649308c2455a41ff
Ghofran Fayyad <148971700+FayadFG@users.noreply.github.com>
2023-10-26 22:43:33 +0300

    Update Program.cs

--- Binary_Search_Homework/Program.cs
+++ Binary_Search_Homework/Program.cs
@@ -9,24 +9,37 @@ namespace Binary_Search_Homework
 {
     internal class Program
     {
-        static void Main(string[] args) // feel free to edit the main method as you like
+         static void Main(string[] args) // feel free to edit the main method as you like
         {
-            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-            int key = 10;
+            int rpet = 1;
+            do
+            {
+                int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+                int key;
+                Console.Write("Enter the number you want to search for =");
+                key = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+                // int idx = BinarySearchHomework.TernarySearch(arr1, key);
+                int idx = BinarySearchHomework.TernarySearch(arr1, key, 0, arr1.Length);
 
-            // int idx = BinarySearchHomework.TernarySearch(arr1, key);
-            int idx = BinarySearchHomework.TernarySearch(arr1, key, 0, arr1.Length);
+                Console.WriteLine("Found {0} at index {1} using Ternary Search", key, idx);
 
-            Console.WriteLine("Found {0} at index {1} using Ternary Search", key, idx);
+                Console.WriteLine("=======================");
 
-            Console.WriteLine("=======================");
+                int[] arr2 = { 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9 };
+                int key2;
+                Console.Write("Enter the number you want to know the number of times it repeats = ");
+                key2 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
 
-            int[] arr2 = { 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 7, 8, 9 };
-            int key2 = 5;
+                int repeat_count = BinarySearchHomework.GetRepeatCount(arr2, key2);
 
-            int repeat_count = BinarySearchHomework.GetRepeatCount(arr2, key2);
+                Console.WriteLine("The number {0} is repeated {1} times", key2, repeat_count);
+                Console.WriteLine();
 
-            Console.WriteLine("the number 2 is repeated {0} times", repeat_count);
+                Console.Write("If You want Exit Press 0 else Press any number =");
+                rpet = Convert.ToInt16(Console.ReadLine());
+            } while (rpet != 0);
         }
     }
 }
